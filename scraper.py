9809eab9f706be0b13249ad9f33ef858e6f9b5d6
ming <mingbao2891@gmail.com>
2023-12-01 13:19:12 +1300

    added stuff

--- scraper.py
+++ scraper.py
@@ -1,20 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 import pypandoc
+import os
 
-init_url = "https://novelfull.com/the-legendary-mechanic/chapter-1-rebirth.html"
+init_url = "https://novelfull.com/everyone-else-is-a-returnee/prologue-part-1.html"
 # chapters = []
 
 # Recursively adds page content to chapters
 # Recursion ends when the next chapter button is disabled
 def save_chapter(url):
     next_url = url
-    count = 100
     
     # Outputs a markdown file of the book
     with open("output.md", "w", encoding="utf-8") as f:
         while (True):
-            #count -= 1 
+        # for i in range(1, 5):
             print(next_url)
             
             # List that stores the data that needs to be written to the markdown file
@@ -29,6 +29,12 @@ def save_chapter(url):
             paragraphs = soup.find_all('p')
             next_chap = soup.find(id="next_chap")
             
+            # get title and write it
+            title = soup.find('span', class_= 'chapter-text')
+            print(title.text)
+            push_string.append('# ' + title.text + '\n\n')
+            
+            # push texts
             for p in paragraphs:
                 text = p.text
                 
@@ -36,10 +42,10 @@ def save_chapter(url):
                 if "Translator:" not in text and "Editor:" not in text and text != "" and "Copyright NovelFull.Com. All Rights Reserved" not in text:
                     text = text.replace("<", "-")
                     text = text.replace(">", "-")
+                    text = text.replace("~", "-")
                     push_string.append(text + "\n\n")
             
             # Makes the first part of the chapter the title
-            push_string[0] = "# " + push_string[0]
             for text in push_string:
                 f.write(text)
             
@@ -49,14 +55,14 @@ def save_chapter(url):
             if 'disabled=' in str(next_chap): return
             
             # Next chapter is available so get the url
-            next_url = "https://novelfull.com" + next_chap['href']
+            next_url = f"https://novelfull.com{next_chap['href']}"
 
 # Saves the book as an epub
 def save_as_epub():
     # Converts the markdown file into a epub using pandoc     
     output = pypandoc.convert_file("output.md", 'epub', outputfile="output.epub")
     assert output == ""
-    
+    # os.remove('output.md')
     
 
 def init():
